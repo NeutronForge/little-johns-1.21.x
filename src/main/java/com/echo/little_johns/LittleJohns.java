9aa2413c5b70f56680a224d7c356c314cbd43832
Echo <71904242+NeutronForge@users.noreply.github.com>
2024-10-26 22:25:43 +0200

    Initial commit

---
+++ src/main/java/com/echo/little_johns/LittleJohns.java
@@ -0,0 +1,16 @@
+package com.echo.little_johns;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LittleJohns implements ModInitializer {
+	public static final String MOD_ID = "little_johns";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/echo/little_johns/LittleJohnsClient.java
@@ -0,0 +1,10 @@
+package com.echo.little_johns;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LittleJohnsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
